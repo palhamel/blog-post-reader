0b4c52186c24047269c225e6563e1411ea30d4ed
Pål Hamel <pal.hamel@gmail.com>
2020-12-30 16:12:54 +0100

    add redux-thunk as middleware in store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
-import jasonPlaceholder from '../apis/jasonPlaceholder'
+import jsonPlaceholder from '../apis/jasonPlaceholder'
 
-export const fetchPosts = () => {
+export const fetchPosts = async () => {
+  const response = await jsonPlaceholder.get('./posts');
+
   return {
     type: "FETCH_POSTS",
+    payload: response
   };
 };
